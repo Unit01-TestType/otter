9cd2d4f9fc86cfb23e3e4ff62d8b2ca88d380ef6
Unit01-TestType <53930035+Unit01-TestType@users.noreply.github.com>
2024-11-28 00:13:32 -0800

    bother

---
+++ bother.py
@@ -0,0 +1,23 @@
+'''
+This script contains the functions that utilize bother to retrieve terrain data
+and create a png and tif raster for the heightmap.
+
+bother: https://github.com/bunburya/bother/tree/master
+
+TODO:
+    -
+'''
+
+
+'''
+bother:
+    - bottom left: lat, long
+    - top right: lat, long
+'''
+
+from subprocess import Popen
+
+def bother():
+    '''
+        - include all both parameters to make a fully functioning wrapper
+    '''
